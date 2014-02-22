4e23a5858a85b7a909b219dfdfd5331f6a4761fa
musicomusio <musicomusio@gmail.com>
2014-02-23 01:10:07 +0900

    Finish sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  include SessionsHelper
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		sign_in @user
+  		flash[:succes] = "Welcome to the Sample App"
+  		redirect_to @user
+  	else
+  		render 'new'
+    end
+  end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe "User Pages" do
 	subject { page }
-
     describe "profile page" do
         let(:user) { FactoryGirl.create(:user) }
 		
@@ -32,9 +31,18 @@ describe "User Pages" do
 				fill_in "Password", with: "foobar"
 				fill_in "Confirmation", with: "foobar"
 			end
-
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count).by(1)
+			end
+
+			describe "after saving the user" do
+				before { click_button submit }
+				let(:user){ User.find_by(email: 'user@example.com')}
+
+				it { should have_link('Sign out') }
+				it { should have_title(user.name) }
+				it { should have_selector('div.alert.alert-success',text: 'Welcome') }
+			  
 			end
 		end
     end
